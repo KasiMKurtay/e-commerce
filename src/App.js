12a7e4546d1952c0250bebff93a4a0153c6953b2
KasiMKurtay <kasmkurtay@outlook.com>
2025-04-05 14:35:59 +0300

    In the Components Footer, NewCollections, NewsLetter added and mount to the Routes

--- src/App.js
+++ src/App.js
@@ -6,34 +6,34 @@ import ShopCategory from "./Pages/ShopCategory";
 import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import LoginSignup from "./Pages/LoginSignup";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        {" "}
         {/*Uygulamanın yönlendirmeleri için BrowserRouter bileşeni kullanılır */}
         <Navbar /> {/*Navbar bileşeni ekleniyor */}
         <Routes>
-          {" "}
           {/*Yönelndirme rotalarını tanımlar */}
-          <Route path="/" element={<Shop />} />{" "}
+          <Route path="/" element={<Shop />} />
           {/*Ana sayfa, Shop bileşenini render eder (Yani ana sayfa Shop olarak gösterilir) */}
-          <Route path="/mens" element={<ShopCategory category="men" />} />{" "}
+          <Route path="/mens" element={<ShopCategory category="men" />} />
           {/* /mens yolu, ShopCategory bileşenini render ve "men" kategorasini render eder */}
           <Route path="/womens" element={<ShopCategory category="women" />} />
           {/* /womens yolu, ShopCategory bileşenini ve "women" kategoreisini render eder */}
-          <Route path="/kids" element={<ShopCategory category="kid" />} />{" "}
+          <Route path="/kids" element={<ShopCategory category="kid" />} />
           {/* /kids yolu, ShopCategory bileşenini render eder */}
           <Route path="product" element={<Product />}>
             {/*Dinamk productId yolu, belirli bir ürün sayfasını render eder */}
             <Route path=":productId" element={<Product />} />
           </Route>
-          <Route path="/cart" element={<Cart />} />{" "}
+          <Route path="/cart" element={<Cart />} />
           {/*Cart yolu, cart bileşenini render eder */}
           <Route path="/login" element={<LoginSignup />} />
           {/* Login yolu, LoginSignup bileşenini render eder */}
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
