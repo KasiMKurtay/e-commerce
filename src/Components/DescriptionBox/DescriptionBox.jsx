f6c18db9b7f19a6ceb6cbb4b35a28cc1c9f8288b
KasiMKurtay <kasmkurtay@outlook.com>
2025-04-06 22:31:57 +0300

    All components done TODO:Add a responsive design

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./DescritpionBox.css";
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (124)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero fugiat
+          perspiciatis numquam iste consectetur nemo ipsam odio suscipit harum
+          nihil. Odio officia officiis excepturi, facilis eos porro nam debitis
+          id ad, ea, exercitationem molestias adipisci in. Nobis voluptatem
+          dolorem eaque velit expedita, qui amet accusantium fugiat? Veniam
+          maxime enim dolorem maiores tenetur iure ipsa reprehenderit nihil
+          facilis adipisci molestias, ipsum dignissimos, suscipit saepe eius sit
+          sed! Vero maxime provident, similique magni ex rem tempore tenetur
+          neque consequuntur molestias.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+          voluptatum rem odit ad optio praesentium sapiente magni exercitationem
+          quaerat quod adipisci, pariatur facere iure dignissimos eligendi quae
+          consequatur alias maiores!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
