89dca3097105248a1dab240ad62e6a44a8b59fcc
KasiMKurtay <kasm.krt44@gmail.com>
2025-04-06 23:41:07 +0300

    Responsive design added for all components &&

--- src/Components/Breadcrums/Breadcrum.jsx
+++ src/Components/Breadcrums/Breadcrum.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import "./Breadcrum.css";
-import arrow_icon from "../Assets/breadcrum_arrow.png";
+import React from "react"; // React kütüphanesini import ediyoruz
+import "./Breadcrum.css"; // CSS stil dosyasını import ediyoruz
+import arrow_icon from "../Assets/breadcrum_arrow.png"; // Ok ikonunu import ediyoruz
 
-const Breadcrum = (props) => {
-  const { product } = props;
+const Breadcrum = (props) => { // Breadcrum fonksiyonel bileşenini tanımlıyoruz
+  const { product } = props; // props'dan product objesini alıyoruz
   return (
-    <div className="breadcrum">
-      HOME <img src={arrow_icon} alt="arrow-icon" /> SHOP
-      <img src={arrow_icon} alt="arrow-icon" />
-      {product.category} <img src={arrow_icon} alt="arrow-icon" />
-      {product.name}
+    <div className="breadcrum"> {/* breadcrum sınıfına sahip div oluşturuyoruz */}
+      HOME <img src={arrow_icon} alt="arrow-icon" /> SHOP {/* Ana sayfa ve shop arasında ok ikonları */}
+      <img src={arrow_icon} alt="arrow-icon" /> {/* Ok ikonunu gösteriyoruz */}
+      {product.category} <img src={arrow_icon} alt="arrow-icon" /> {/* Ürün kategorisini ve ok ikonunu ekliyoruz */}
+      {product.name} {/* Ürün adını ekliyoruz */}
     </div>
   );
 };
 
-export default Breadcrum;
+export default Breadcrum; // Breadcrum bileşenini dışa aktarıyoruz
--- src/Components/Offers/Offers.jsx
+++ src/Components/Offers/Offers.jsx
@@ -8,7 +8,7 @@ const Offers = () => {
       <div className="offers-left">
         <h1>Exclusive</h1>
         <h1>Offers For You</h1>
-        <p>ONLY ON BEST SELLER PRODUCTS</p>
+        <p className="onlyon">ONLY ON BEST SELLER PRODUCTS</p>
         <button>Check Now</button>
       </div>
       <div className="offers-right">
