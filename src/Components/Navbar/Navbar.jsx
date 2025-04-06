75fbac4bacc93443cc55d0f1501cfeb29970164f
KasiMKurtay <kasm.krt44@gmail.com>
2025-04-06 22:31:57 +0300

    All components done TODO:Add a responsive design

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./Navbar.css";
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
 import { Link } from "react-router-dom";
+import { ShopContext } from "../../Context/ShopContext";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("shop"); //Menüde hangi sekmenin seçili olduğunu gösteren state
-
+  const { getTotalCartItems } = useContext(ShopContext);
   return (
     <div className="navbar">
       <div className="nav-logo">
@@ -68,7 +69,7 @@ const Navbar = () => {
         <Link to="/cart">
           <img src={cart_icon} alt="cart-icon" />
         </Link>
-        <div className="nav-cart-count">0</div>{" "}
+        <div className="nav-cart-count">{getTotalCartItems()}</div>{" "}
         {/*Sepet içindeki ürün sayısını gösteren alan */}
       </div>
     </div>
